e4485c77dbda9ecbe93c057ae51dc395e0138894
jimmy094 <jimmyalvarezjr@gmail.com>
2023-02-19 21:39:30 -0500

    hardhat

--- scripts/run.js
+++ scripts/run.js
@@ -8,33 +8,36 @@ const main = async () => {
 
     //⬇️ hardhat creating a local eth network just for this contract
     const waveContract = await waveContractFactory.deploy();
-
     //⬇️ Will deploy contract
     await waveContract.deployed();
+
     
     console.log("Contract deployed to:", waveContract.address);
-
-    console.log("Contract deployed to:", waveContract.address);
     console.log("Contract deployed by:", owner.address);
+   
+    await waveContract.getTotalWaves();
+
+    const firstWaveTxn = await waveContract.wave();
+    await firstWaveTxn.wait();
 
     await waveContract.getTotalWaves();
 
-    const waveTxn = await waveContract.wave();
-    await waveTxn.wait();
-
+    const secondWaveTxn = await waveContract.connect(randomPerson).wave();
+    await secondWaveTxn.wait()
+    
     await waveContract.getTotalWaves();
-    };
-  };
-  
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0); // exit Node process without error
-    } catch (error) {
-      console.log(error);
-      process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
-    }
-    // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
-  };
-  
-  runMain();
+    
+};
+
+const runMain = async () => {
+try {
+    await main();
+    process.exit(0); // exit Node process without error
+} catch (error) {
+    console.log(error);
+    process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
+}
+// Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
+};
+
+runMain();
